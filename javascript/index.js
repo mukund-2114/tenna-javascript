5b01c093fbb8770c521d9b3547b331fafc749abb
mukund-2114 <mdrkkapadia@gmail.com>
2025-11-24 11:11:17 -0500

    initial

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+console.log("Hello, World!");
+
+document.getElementById("hello").style.color = "blue";
+
+
+
+
+document.getElementsByClassName("common")[1].style.backgroundColor = "green"
+
+
+document.getElementsByTagName("h1")[1].style.color = "red";
+
+
+document.querySelector(".common").style.fontSize = "40px";
+
+
+function changeColor(){
+    console.log("Button clicked!");
+document.getElementById("hello").innerText = "Hello, JavaScript!";
+
+    document.getElementById("hello").style.backgroundColor = "yellow";
+}
