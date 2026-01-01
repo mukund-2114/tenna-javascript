b4e282ff80ea029c593279366be7f4a2fb0d35b4
mukund-2114 <mdrkkapadia@gmail.com>
2026-01-01 12:08:38 -0500

    weather app

---
+++ weather/index.js
@@ -0,0 +1,28 @@
+console.log("weather app")
+
+const API_KEY = "1d86a56c12422ae014c72ac0bc807215"
+
+const city = document.getElementById("weatherInput")
+const tempOutput = document.getElementById("temp")
+
+
+
+async function getWeatherDetais(){
+    console.log("fetching weather details.....")
+   
+    if(city.value == ""){
+        alert("Please enter a city name")
+        throw new Error("City is empty")
+    }
+
+    try {
+    const data =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${API_KEY}&units=metric`)
+    const response = await data.json()
+    const temperature =response.main.temp
+    console.log("Data received", response)
+    // alert(`Weather of ${city.value} is ${temperature}`)
+   tempOutput.innerText = `Weather of ${city.value} is ${temperature}`
+    } catch (error) {
+        console.log(error)
+    }
+}
